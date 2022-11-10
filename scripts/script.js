1214033dabd4e40bce056832f97f78ee30e8fbb7
Daniel Lazzaroni <dlazzaroni@my.bcit.ca>
2022-11-09 20:57:18 -0800

    created do/while loop

--- scripts/script.js
+++ scripts/script.js
@@ -3,3 +3,12 @@ const courseList = {
     code: "MATH 1320" ,  name:  "Technical Math for IT",
     code: "ACIT 1630" ,  name:  "Database Systems"
 }
+
+let input = ""
+do{
+    input = prompt("Enter a BCIT course code (4-digit number): ");
+}
+while(input.length != 4 || isNaN(Number(input)))
+
+
+
