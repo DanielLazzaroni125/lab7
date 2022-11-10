a4fbb11ff759c9d4e2b28680ef774e5a1d0d29ba
Daniel Lazzaroni <dlazzaroni@my.bcit.ca>
2022-11-09 19:45:45 -0800

    creates courseList array

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,5 @@
 const courseList = {
     code: "ACIT 1620" ,  name:  "Web Fundamental Technologies",
-    
+    code: "MATH 1320" ,  name:  "Technical Math for IT",
+    code: "ACIT 1630" ,  name:  "Database Systems"
 }
