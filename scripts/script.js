84f46e336833714794038eb3f1d242621ef3b2c1
Daniel Lazzaroni <dlazzaroni@my.bcit.ca>
2022-11-10 19:01:53 -0800

    created for of loop with nested if statement

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,23 @@
-const courseList = {
-    code: "ACIT 1620" ,  name:  "Web Fundamental Technologies",
-    code: "MATH 1320" ,  name:  "Technical Math for IT",
-    code: "ACIT 1630" ,  name:  "Database Systems"
-}
+const courseList = [
+    {code: "ACIT 1620" ,  name:  "Web Fundamental Technologies"},
+    {code: "ACIT 1515" ,  name:  "Scripting for IT"},
+    {code: "ACIT 1630" ,  name:  "Database Systems"}
+]
 
 let input = ""
-do{
+do{ 
     input = prompt("Enter a BCIT course code (4-digit number): ");
 }
 while(input.length != 4 || isNaN(Number(input)))
 
 
+for (let course of courseList){
+    if (course.code.includes(input)){
+        console.log(`Yes I am taking the course: ${course.code} - ${course.name}`)
+    } else{
+        let newCode = {code: "ACIT " + input, name: null};
+        courseList.push(newCode)
+        console.log("Success")
+    }
+}
 
